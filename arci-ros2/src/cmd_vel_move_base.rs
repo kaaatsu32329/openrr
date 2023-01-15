fa549dc3f50fe8318e888d323e78565d3fc24067
Katsu Uchiyama <katsu1112uchi@gmail.com>
2023-01-15 14:25:35 +0900

    [arci-ros2] Implement `safe_drive` version

--- arci-ros2/src/cmd_vel_move_base.rs
+++ arci-ros2/src/cmd_vel_move_base.rs
@@ -1,15 +1,28 @@
 use arci::*;
 use parking_lot::Mutex;
-use r2r::geometry_msgs::msg::Twist;
+#[cfg(feature = "r2r")]
+use r2r::geometry_msgs::msg::Twist as r2r_Twist;
+#[cfg(all(feature = "safe_drive", not(feature = "r2r")))]
+use safe_drive::msg::common_interfaces::geometry_msgs::msg::Twist as safe_drive_Twist;
 use serde::{Deserialize, Serialize};
+#[cfg(all(feature = "safe_drive", not(feature = "r2r")))]
+use std::sync::Arc;
 
 /// Implement arci::MoveBase for ROS2
+#[cfg(feature = "r2r")]
 pub struct Ros2CmdVelMoveBase {
-    vel_publisher: Mutex<r2r::Publisher<Twist>>,
+    vel_publisher: Mutex<r2r::Publisher<r2r_Twist>>,
     // keep not to be dropped
     _node: Mutex<r2r::Node>,
 }
 
+#[cfg(all(feature = "safe_drive", not(feature = "r2r")))]
+pub struct Ros2CmdVelMoveBase {
+    vel_publisher: Mutex<safe_drive::topic::publisher::Publisher<safe_drive_Twist>>,
+    _node: Mutex<safe_drive::node::Node>,
+}
+
+#[cfg(feature = "r2r")]
 impl Ros2CmdVelMoveBase {
     /// Creates a new `Ros2CmdVelMoveBase` from ROS2 context and Twist topic name.
     #[track_caller]
@@ -32,9 +45,33 @@ impl Ros2CmdVelMoveBase {
     }
 }
 
+#[cfg(all(feature = "safe_drive", not(feature = "r2r")))]
+impl Ros2CmdVelMoveBase {
+    /// Creates a new `Ros2CmdVelMoveBase` from ROS2 context and Twist topic name.
+    #[track_caller]
+    pub fn new(ctx: Arc<safe_drive::context::Context>, cmd_topic_name: &str) -> Self {
+        // TODO: Consider using unique name
+        let node = ctx.create_node("cmd_vel_node", None, Default::default()).unwrap();
+        Self::from_node(node, cmd_topic_name)
+    }
+
+    /// Creates a new `Ros2CmdVelMoveBase` from ROS2 node and Twist topic name.
+    #[track_caller]
+    pub fn from_node(mut node: Arc<safe_drive::node::Node>, cmd_topic_name: &str) -> Self {
+        Self {
+            vel_publisher: Mutex::new(
+                node.create_publisher::<safe_drive_Twist>(cmd_topic_name, None)
+                    .unwrap(),
+            ),
+            _node: Mutex::new(*node.as_ref()),
+        }
+    }
+}
+
+#[cfg(feature = "r2r")]
 impl MoveBase for Ros2CmdVelMoveBase {
     fn send_velocity(&self, velocity: &BaseVelocity) -> Result<(), Error> {
-        let mut twist_msg = Twist::default();
+        let mut twist_msg = r2r_Twist::default();
         twist_msg.linear.x = velocity.x;
         twist_msg.linear.y = velocity.y;
         twist_msg.angular.z = velocity.theta;
@@ -43,6 +80,26 @@ impl MoveBase for Ros2CmdVelMoveBase {
             .publish(&twist_msg)
             .map_err(|e| arci::Error::Connection {
                 message: format!("r2r publish error: {e:?}"),
+            })
+    }
+
+    fn current_velocity(&self) -> Result<BaseVelocity, Error> {
+        unimplemented!("Read from /odom in the future?");
+    }
+}
+
+#[cfg(all(feature = "safe_drive", not(feature = "r2r")))]
+impl MoveBase for Ros2CmdVelMoveBase {
+    fn send_velocity(&self, velocity: &BaseVelocity) -> Result<(), Error> {
+        let mut twist_msg = safe_drive_Twist::new().unwrap();
+        twist_msg.linear.x = velocity.x;
+        twist_msg.linear.y = velocity.y;
+        twist_msg.angular.z = velocity.theta;
+        self.vel_publisher
+            .lock()
+            .send(&twist_msg)
+            .map_err(|e| arci::Error::Connection {
+                message: format!("safe_drive publish error: {e:?}"),
             })
     }
 
